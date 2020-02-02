5c781631c355b7b1e93d4629a240b8358df741a8
Luiz Rioja <lulyrioja@gmail.com>
2020-02-01 23:01:17 -0300

    Teste

--- tindev/src/routes.js
+++ tindev/src/routes.js
@@ -1,6 +1,7 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import Login from './pages/Login';
+
 import Main from './pages/Main';
 
 export default createAppContainer(
