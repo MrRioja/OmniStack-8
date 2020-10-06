05675702e3f895428932ac1bc78b60bccae0e230
Luiz Rioja <lulyrioja@gmail.com>
2020-10-05 23:59:18 -0300

    :alien: Atualizando projeto Tindev

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,32 +1,36 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const routes = require('./routes');
+const routes = require("./routes");
 
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
 const connectedUsers = {};
 
-io.on('connection', socket => {
-    const { user } = socket.handshake.query;
+io.on("connection", (socket) => {
+  const { user } = socket.handshake.query;
 
-    console.log(user, socket.id)
+  console.log(user, socket.id);
 
-    connectedUsers[user] = socket.id;
+  connectedUsers[user] = socket.id;
 });
 
-mongoose.connect('mongodb+srv://lrioja:lrioja@cluster0-jxg5l.mongodb.net/omnistack8?retryWrites=true&w=majority', {
-    useNewUrlParser: true
-})
+mongoose.connect(
+  "mongodb+srv://lrioja:lriojaOmniStack8@cluster0-jxg5l.mongodb.net/omnistack8?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 app.use((req, res, next) => {
-    req.io = io;
-    req.connectedUsers = connectedUsers;
+  req.io = io;
+  req.connectedUsers = connectedUsers;
 
-    return next();
+  return next();
 });
 
 app.use(cors());
